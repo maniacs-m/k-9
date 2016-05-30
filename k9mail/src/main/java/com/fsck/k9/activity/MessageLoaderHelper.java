a1eb9c73fc220dd13fc555ea2bde7b2ed45af374
Vincent Breitmoser <look@my.amazin.horse>
2016-05-30 17:18:12 +0200

    move all loading logic into MessageLoaderHelper (breaks MessageCompose)

---
+++ k9mail/src/main/java/com/fsck/k9/activity/MessageLoaderHelper.java
@@ -0,0 +1,326 @@
+package com.fsck.k9.activity;
+
+
+import android.app.FragmentManager;
+import android.app.LoaderManager;
+import android.app.LoaderManager.LoaderCallbacks;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.content.Loader;
+import android.os.Bundle;
+import android.support.annotation.UiThread;
+
+import com.fsck.k9.Account;
+import com.fsck.k9.Preferences;
+import com.fsck.k9.controller.MessagingController;
+import com.fsck.k9.controller.MessagingListener;
+import com.fsck.k9.helper.RetainFragment;
+import com.fsck.k9.mail.Flag;
+import com.fsck.k9.mailstore.LocalMessage;
+import com.fsck.k9.mailstore.MessageViewInfo;
+import com.fsck.k9.ui.crypto.MessageCryptoAnnotations;
+import com.fsck.k9.ui.crypto.MessageCryptoCallback;
+import com.fsck.k9.ui.crypto.MessageCryptoHelper;
+import com.fsck.k9.ui.message.LocalMessageExtractorLoader;
+import com.fsck.k9.ui.message.LocalMessageLoader;
+
+
+public class MessageLoaderHelper {
+
+    private static final int LOCAL_MESSAGE_LOADER_ID = 1;
+    private static final int DECODE_MESSAGE_LOADER_ID = 2;
+
+
+    // injected state
+    private final Context context;
+    private final FragmentManager fragmentManager;
+    private final LoaderManager loaderManager;
+    private final MessageLoaderCallbacks view;
+
+
+    // transient state
+    private MessageReference messageReference;
+    private Account account;
+
+    private LocalMessage localMessage;
+    private MessageCryptoAnnotations messageCryptoAnnotations;
+
+    private MessageCryptoHelper messageCryptoHelper;
+    private RetainFragment<MessageCryptoHelper> retainCryptoHelperFragment;
+
+
+    public MessageLoaderHelper(Context context, LoaderManager loaderManager, FragmentManager fragmentManager,
+            MessageLoaderCallbacks view) {
+        this.context = context;
+        this.loaderManager = loaderManager;
+        this.fragmentManager = fragmentManager;
+        this.view = view;
+    }
+
+
+    // public interface
+
+    @UiThread
+    public void asyncStartOrResumeLoadingMessage(MessageReference messageReference) {
+        this.messageReference = messageReference;
+        this.account = Preferences.getPreferences(context).getAccount(messageReference.getAccountUuid());
+
+        startOrResumeLocalMessageLoader();
+    }
+
+    @UiThread
+    public void onDestroy() {
+        cancelAndClearLocalMessageLoader();
+        cancelAndClearDecodeLoader();
+        cancelAndClearCryptoOperation();
+    }
+
+    @UiThread
+    public void onDestroyChangingConfigurations() {
+        if (messageCryptoHelper != null) {
+            messageCryptoHelper.detachCallback();
+        }
+    }
+
+    @UiThread
+    public void restartMessageCryptoProcessing() {
+        cancelAndClearCryptoOperation();
+        // TODO
+        // mMessageView.setToLoadingState();
+        startOrResumeCryptoOperation();
+    }
+
+    @UiThread
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        messageCryptoHelper.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @UiThread
+    public void onClickDownloadCompleteMessage() {
+        if (localMessage.isSet(Flag.X_DOWNLOADED_FULL)) {
+            return;
+        }
+
+        startDownloadingMessageBody(true);
+    }
+
+
+    // load from database
+
+    private void startOrResumeLocalMessageLoader() {
+        loaderManager.initLoader(LOCAL_MESSAGE_LOADER_ID, null, localMessageLoaderCallback);
+    }
+
+    @UiThread
+    private void onLoadMessageFromDatabaseFinished() {
+        view.onMessageLoadFinished(localMessage);
+
+        if (localMessage.isBodyMissing()) {
+            startDownloadingMessageBody(false);
+            return;
+        }
+
+        if (account.isOpenPgpProviderConfigured()) {
+            startOrResumeCryptoOperation();
+            return;
+        }
+
+        startOrResumeDecodeMessage();
+    }
+
+    private void onLoadMessageFromDatabaseFailed() {
+        view.onMessageLoadFailed();
+    }
+
+    private void cancelAndClearLocalMessageLoader() {
+        loaderManager.destroyLoader(LOCAL_MESSAGE_LOADER_ID);
+    }
+
+    private LoaderCallbacks<LocalMessage> localMessageLoaderCallback = new LoaderCallbacks<LocalMessage>() {
+        @Override
+        public Loader<LocalMessage> onCreateLoader(int id, Bundle args) {
+            if (id != LOCAL_MESSAGE_LOADER_ID) {
+                throw new IllegalStateException("loader id must be message loader id");
+            }
+
+            return new LocalMessageLoader(context, MessagingController.getInstance(context), account, messageReference);
+        }
+
+        @Override
+        public void onLoadFinished(Loader<LocalMessage> loader, LocalMessage message) {
+            if (loader.getId() != LOCAL_MESSAGE_LOADER_ID) {
+                throw new IllegalStateException("loader id must be message loader id");
+            }
+
+            localMessage = message;
+            if (message == null) {
+                onLoadMessageFromDatabaseFailed();
+            } else {
+                onLoadMessageFromDatabaseFinished();
+            }
+            loaderManager.destroyLoader(LOCAL_MESSAGE_LOADER_ID);
+        }
+
+        @Override
+        public void onLoaderReset(Loader<LocalMessage> loader) {
+            if (loader.getId() != LOCAL_MESSAGE_LOADER_ID) {
+                throw new IllegalStateException("loader id must be message loader id");
+            }
+            // Do nothing
+        }
+    };
+
+
+    // process with crypto helper
+
+    private void startOrResumeCryptoOperation() {
+        retainCryptoHelperFragment =
+                RetainFragment.findOrCreate(fragmentManager, "crypto_helper_" + localMessage.hashCode());
+        if (retainCryptoHelperFragment.hasData()) {
+            messageCryptoHelper = retainCryptoHelperFragment.getData();
+        } else {
+            messageCryptoHelper = new MessageCryptoHelper(context, account.getOpenPgpProvider());
+            retainCryptoHelperFragment.setData(messageCryptoHelper);
+        }
+        messageCryptoHelper.asyncStartOrResumeProcessingMessage(localMessage, messageCryptoCallback);
+    }
+
+    private void cancelAndClearCryptoOperation() {
+        if (messageCryptoHelper != null) {
+            messageCryptoHelper.cancelIfRunning();
+            messageCryptoHelper = null;
+        }
+        if (retainCryptoHelperFragment != null) {
+            retainCryptoHelperFragment.clearAndRemove(fragmentManager);
+            retainCryptoHelperFragment = null;
+        }
+    }
+
+    private MessageCryptoCallback messageCryptoCallback = new MessageCryptoCallback() {
+        @Override
+        public void onCryptoHelperProgress(int current, int max) {
+            view.setLoadingProgress(current, max);
+        }
+
+        @Override
+        public void onCryptoOperationsFinished(MessageCryptoAnnotations annotations) {
+            messageCryptoAnnotations = annotations;
+            startOrResumeDecodeMessage();
+        }
+
+        @Override
+        public void startPendingIntentForCryptoHelper(IntentSender si, int requestCode, Intent fillIntent,
+                int flagsMask, int flagValues, int extraFlags) {
+            view.startIntentSenderForMessageLoaderHelper(si, requestCode, fillIntent,
+                    flagsMask, flagValues, extraFlags);
+        }
+    };
+
+
+    // decode message
+
+    private void startOrResumeDecodeMessage() {
+        loaderManager.initLoader(DECODE_MESSAGE_LOADER_ID, null, decodeMessageLoaderCallback);
+    }
+
+    private void onDecodeMessageFinished(MessageViewInfo messageViewInfo) {
+        if (messageViewInfo == null) {
+            view.onMessageViewInfoLoadFailed(localMessage);
+            return;
+        }
+
+        view.onMessageViewInfoLoadFinished(localMessage, messageViewInfo);
+    }
+
+    private void cancelAndClearDecodeLoader() {
+        loaderManager.destroyLoader(DECODE_MESSAGE_LOADER_ID);
+    }
+
+    private LoaderCallbacks<MessageViewInfo> decodeMessageLoaderCallback = new LoaderCallbacks<MessageViewInfo>() {
+        @Override
+        public Loader<MessageViewInfo> onCreateLoader(int id, Bundle args) {
+            if (id != DECODE_MESSAGE_LOADER_ID) {
+                throw new IllegalStateException("loader id must be message decoder id");
+            }
+            return new LocalMessageExtractorLoader(context, localMessage, messageCryptoAnnotations);
+        }
+
+        @Override
+        public void onLoadFinished(Loader<MessageViewInfo> loader, MessageViewInfo messageViewInfo) {
+            if (loader.getId() != DECODE_MESSAGE_LOADER_ID) {
+                throw new IllegalStateException("loader id must be message decoder id");
+            }
+            onDecodeMessageFinished(messageViewInfo);
+        }
+
+        @Override
+        public void onLoaderReset(Loader<MessageViewInfo> loader) {
+            if (loader.getId() != DECODE_MESSAGE_LOADER_ID) {
+                throw new IllegalStateException("loader id must be message decoder id");
+            }
+            // Do nothing
+        }
+    };
+
+
+    // download missing body
+
+    private void startDownloadingMessageBody(boolean downloadComplete) {
+        if (downloadComplete) {
+            MessagingController.getInstance(context).loadMessageRemote(
+                    account, messageReference.getFolderName(), messageReference.getUid(), downloadMessageListener);
+        } else {
+            MessagingController.getInstance(context).loadMessageRemotePartial(
+                    account, messageReference.getFolderName(), messageReference.getUid(), downloadMessageListener);
+        }
+    }
+
+    private void onMessageDownloadFinished() {
+        cancelAndClearLocalMessageLoader();
+        cancelAndClearDecodeLoader();
+        cancelAndClearCryptoOperation();
+
+        startOrResumeLocalMessageLoader();
+    }
+
+    private void onDownloadMessageFailed(final Throwable t) {
+        if (t instanceof IllegalArgumentException) {
+            view.onDownloadErrorMessageNotFound();
+        } else {
+            view.onDownloadErrorNetworkError();
+        }
+    }
+
+    MessagingListener downloadMessageListener = new MessagingListener() {
+        @Override
+        public void loadMessageRemoteFinished(Account account, String folder, String uid) {
+            onMessageDownloadFinished();
+        }
+
+        @Override
+        public void loadMessageRemoteFailed(Account account, String folder, String uid, final Throwable t) {
+            onDownloadMessageFailed(t);
+        }
+    };
+
+
+    // callback interface
+
+    public interface MessageLoaderCallbacks {
+        void onMessageLoadFinished(LocalMessage message);
+        void onMessageLoadFailed();
+
+        void onMessageViewInfoLoadFinished(LocalMessage localMessage, MessageViewInfo messageViewInfo);
+        void onMessageViewInfoLoadFailed(LocalMessage localMessage);
+
+        void setLoadingProgress(int current, int max);
+
+        void startIntentSenderForMessageLoaderHelper(IntentSender si, int requestCode, Intent fillIntent, int flagsMask,
+                int flagValues, int extraFlags);
+
+        void onDownloadErrorMessageNotFound();
+        void onDownloadErrorNetworkError();
+    }
+
+}
